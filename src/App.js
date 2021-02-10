8eae0dda9818ace1c286b723a354b4dbd2f90f63
Tom Runnels <tjrunnels@liberty.edu>
2021-02-10 15:09:05 -0500

    added api integration

--- src/App.js
+++ src/App.js
@@ -1,41 +1,102 @@
-import React, { Component } from 'react';
+//default
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//cognito authentication 
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+
+//appsync api
+import {API} from 'aws-amplify';
+import {listPrivateNotes} from './graphql/queries'
+import {createPrivateNote, deletePrivateNote} from './graphql/mutations'
+
+
+//after imports
 Amplify.configure(aws_exports);
 
 
+//initials
+const initialFormState = {content: ''}
 
-class App extends Component {
 
-  state = {}
+function App2() {
+  const [notes, setNotes] = useState([])
+  const [formData, setFormData] = useState(initialFormState)
+  const [currentUser, setCurrentUser] = useState('not-signed-in')
 
-  async componentDidMount() {
-    this.renderUser();
-  }
+  useEffect(() => {
+   fetchMyNotes();
+   updateAWSUser();
+  }, [])
 
-  renderUser = async() => {
+  async function updateAWSUser() {
     try{
       let user = await Auth.currentAuthenticatedUser();
       console.log(user.username)
-      this.setState( {User: user.username})
+      setCurrentUser(user.username)
     } catch (err) {
       console.log ("error", err)
     }
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h3>get gud {this.state.User}</h3>
-        </header> 
-      </div>
-    );
+
+  async function fetchMyNotes() {
+    const apiData = await API.graphql({query: listPrivateNotes});
+    setNotes(apiData.data.listPrivateNotes.items);
   }
+
+  async function createNote() {
+    if (!formData.content) return;
+    await API.graphql({ query: createPrivateNote, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deletePrivateNote, variables: { input: { id } }});
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <div style={{flex:1}}>
+              <h1>{currentUser}'s Notes </h1>
+              <input
+                onChange={e => setFormData({ ...formData, 'content': e.target.value})}
+                placeholder="Note"
+                value={formData.content}
+              />
+
+              <button onClick={createNote}>Create Note</button>
+              <div style={{marginBottom: 30}}>
+                {
+                  notes.map(note => (
+                    <div key={note.id || note.content} style={{ border: '4px dotted lightblue'}}>
+                      <h2>{note.content}</h2>
+
+                      <button onClick={() => deleteNote(note)}>Delete note</button>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+        </div>
+            
+          
+      </header> 
+    </div>
+  );
+
 }
 
-export default withAuthenticator(App, true);
+
+
+export default withAuthenticator(App2, true);
